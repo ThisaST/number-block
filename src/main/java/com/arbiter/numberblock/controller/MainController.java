6e471fe7109ef09f9b88409c4645b2c88d896133
Thisara <thisa030@gmail.com>
2019-07-05 02:38:44 +0530

    added buttons to index

--- src/main/java/com/arbiter/numberblock/controller/MainController.java
+++ src/main/java/com/arbiter/numberblock/controller/MainController.java
@@ -7,9 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
 
+    int[] values = {1, 2, 3, 4, 5, 6, 7};
     @RequestMapping(value = {"/", "/index", "/index.html"})
     public String index(Model model) {
-        model.addAttribute("message", "NUMBER");
+        model.addAttribute("values", values);
 
         return "index";
     }
